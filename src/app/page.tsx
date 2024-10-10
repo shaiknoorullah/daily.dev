9b68ddd20cb2ce1ce6208d0b50945bb4191ef81d
aymanKhan <akhan@proficientnowtech.com>
2024-10-10 18:54:52 +0530

    minorChanges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Sidebar from "./components/layouts/asideBar";
 import Feed from "./components/layouts/feed";
 import NavBar from "./components/layouts/navbar";
+import { getUser } from "./utils/auth";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const user = getUser();
+    if (!user) {
+      router.push("/Login");
+    }
+  }, [router]);
+
   return (
     <div className="">
       <NavBar />
